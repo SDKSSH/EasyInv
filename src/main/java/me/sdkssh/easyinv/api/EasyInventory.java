1a699318fea577ec04d891a45024878b76143380
SDKSSH <sdk_ssh@protonmail.com>
2022-06-19 20:43:01 +0200

    Init constructor

--- src/main/java/me/sdkssh/easyinv/api/EasyInventory.java
+++ src/main/java/me/sdkssh/easyinv/api/EasyInventory.java
@@ -1,4 +1,15 @@
 package me.sdkssh.easyinv.api;
 
+import lombok.Getter;
+
 public abstract class EasyInventory {
+    @Getter
+    private int rows;
+    @Getter
+    private String inventoryName;
+
+    public EasyInventory(String name, int rows){
+        this.inventoryName = name;
+        this.rows = rows;
+    }
 }
