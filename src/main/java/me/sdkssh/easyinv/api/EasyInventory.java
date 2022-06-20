06f2142c0bb35f25527b6ec169440c6088e581bd
SDKSSH <sdk_ssh@protonmail.com>
2022-06-20 08:48:43 +0200

    Add inventory variable

--- src/main/java/me/sdkssh/easyinv/api/EasyInventory.java
+++ src/main/java/me/sdkssh/easyinv/api/EasyInventory.java
@@ -1,15 +1,22 @@
 package me.sdkssh.easyinv.api;
 
 import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 public abstract class EasyInventory {
     @Getter
     private int rows;
     @Getter
     private String inventoryName;
+    @Getter
+    private Inventory inventory;
 
     public EasyInventory(String name, int rows){
         this.inventoryName = name;
         this.rows = rows;
     }
+
+    public abstract void onOpen(Player player);
+    
 }
