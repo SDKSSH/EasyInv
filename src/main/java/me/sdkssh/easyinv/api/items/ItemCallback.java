a6a77d677b0227ef134928da8f773215a3553850
SDKSSH <sdk_ssh@protonmail.com>
2022-06-20 09:05:28 +0200

    Add "onExecute"

--- src/main/java/me/sdkssh/easyinv/api/items/ItemCallback.java
+++ src/main/java/me/sdkssh/easyinv/api/items/ItemCallback.java
@@ -1,4 +1,11 @@
 package me.sdkssh.easyinv.api.items;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
 public abstract class ItemCallback {
+    public abstract void onExecute(Player p, Inventory inv, ItemStack itemStack, Action action);
+    
 }
