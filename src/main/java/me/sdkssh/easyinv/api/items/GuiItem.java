ddd20139a7a7971b3362d691c3d9edcb2d732e81
SDKSSH <sdk_ssh@protonmail.com>
2022-06-20 09:12:25 +0200

    Finish ItemCallback and guiitem init constructor

--- src/main/java/me/sdkssh/easyinv/api/items/GuiItem.java
+++ src/main/java/me/sdkssh/easyinv/api/items/GuiItem.java
@@ -3,7 +3,13 @@ package me.sdkssh.easyinv.api.items;
 import lombok.Getter;
 import org.bukkit.inventory.ItemStack;
 
+@Getter
 public class GuiItem {
-    @Getter
+    private ItemCallback callback;
     private ItemStack itemStack;
+
+    public GuiItem(ItemStack it){
+        this.itemStack = it;
+        this.callback = null;
+    }
 }
--- src/main/java/me/sdkssh/easyinv/api/items/ItemCallback.java
+++ src/main/java/me/sdkssh/easyinv/api/items/ItemCallback.java
@@ -1,11 +1,15 @@
 package me.sdkssh.easyinv.api.items;
 
 import org.bukkit.entity.Player;
-import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public abstract class ItemCallback {
-    public abstract void onExecute(Player p, Inventory inv, ItemStack itemStack, Action action);
-    
+    public abstract void onExecute(Player p, Inventory inv, ItemStack itemStack, InventoryAction action);
+
+    public void execute(InventoryClickEvent e){
+        onExecute((Player) e.getWhoClicked(), e.getInventory(), e.getCurrentItem(), e.getAction());
+    }
 }
