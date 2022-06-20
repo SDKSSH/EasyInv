edb34f52a0d34bff45557bffac276a636e3c4d92
SDKSSH <sdk_ssh@protonmail.com>
2022-06-20 09:13:51 +0200

    Add map of integer and GuiItem

--- src/main/java/me/sdkssh/easyinv/api/EasyInventory.java
+++ src/main/java/me/sdkssh/easyinv/api/EasyInventory.java
@@ -1,8 +1,12 @@
 package me.sdkssh.easyinv.api;
 
 import lombok.Getter;
+import me.sdkssh.easyinv.api.items.GuiItem;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class EasyInventory {
     @Getter
@@ -11,10 +15,13 @@ public abstract class EasyInventory {
     private String inventoryName;
     @Getter
     private Inventory inventory;
+    @Getter
+    private Map<Integer, GuiItem> items;
 
     public EasyInventory(String name, int rows){
         this.inventoryName = name;
         this.rows = rows;
+        this.items = new HashMap<>();
     }
 
     public abstract void onOpen(Player player);
